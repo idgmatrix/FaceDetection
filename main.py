f798ce792a1dc5d1a0a050002f159f5a33e76503
SeongWan Kim <idgmatrix@gmail.com>
2021-08-11 15:38:31 +0900

    frontal face detection

--- main.py
+++ main.py
@@ -14,8 +14,6 @@ color_green = (0, 255, 0)
 color_blue = (255, 0, 0)
 line_width = 2
 
-
-
 while True:
     ret_val, img = cam.read()
     img = cv2.flip(img, 1)
@@ -36,18 +34,32 @@ while True:
             color = color_red
             rect_color = color_red
 
-        if abs(x5 - (x1 + x3) / 2) > 2.5:
+        if abs(x5 - (x2 + x4) / 2) > 2:
             color = color_red
             rect_color = color_red
+
+        eye_width = abs(x1 - x3)
+        eye_height = abs((y1 + y3) / 2 - y5)
+        ratio = eye_height / eye_width
+        text = '{}'.format(ratio)
+        if ratio < 0.5:
+            color = color_red
+            rect_color = color_red
+
+
+        cv2.putText(img, text, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.8,
+                    color, 1, cv2.LINE_AA)
 
         cv2.line(img, (x1, y1), (x2, y2), color, line_width)
         cv2.line(img, (x2, y2), (x5, y5), color, line_width)
         cv2.line(img, (x5, y5), (x4, y4), color, line_width)
         cv2.line(img, (x4, y4), (x3, y3), color, line_width)
         cv2.rectangle(img, (det.left(), det.top()), (det.right(), det.bottom()), rect_color, line_width)
+        cx, cy = (det.left() + det.right()) / 2, (det.top() + det.bottom()) / 2
+        cv2.circle(img, (int(cx), int(cy)), 2, color)
         #break
 
-    cv2.imshow('Face Detactor', img)
+    cv2.imshow('Face Detector', img)
     if cv2.waitKey(1) == 27:
         break  # esc to quit
 cv2.destroyAllWindows()
