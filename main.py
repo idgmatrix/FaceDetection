5c301ad721c5213b70850402cded25ecc7de4afa
SeongWan Kim <idgmatrix@gmail.com>
2021-08-11 12:53:14 +0900

    frontal face detection

--- main.py
+++ main.py
@@ -8,9 +8,14 @@ detector = dlib.get_frontal_face_detector()
 sp = dlib.shape_predictor(predictor_path)
 
 cam = cv2.VideoCapture(0)
+# OpenCV color format: BGR
+color_red = (0, 0, 255)
 color_green = (0, 255, 0)
 color_blue = (255, 0, 0)
 line_width = 2
+
+
+
 while True:
     ret_val, img = cam.read()
     img = cv2.flip(img, 1)
@@ -19,20 +24,30 @@ while True:
     #faces = dlib.full_object_detections()
     for det in dets:
         shape = sp(img, det)
-        cv2.rectangle(img, (det.left(), det.top()), (det.right(), det.bottom()), color_green, line_width)
         x1, y1 = shape.part(0).x, shape.part(0).y
         x2, y2 = shape.part(1).x, shape.part(1).y
         x3, y3 = shape.part(2).x, shape.part(3).y
         x4, y4 = shape.part(3).x, shape.part(3).y
         x5, y5 = shape.part(4).x, shape.part(4).y
-        cv2.line(img, (x1, y1), (x2, y2), color_blue, line_width)
-        cv2.line(img, (x2, y2), (x5, y5), color_blue, line_width)
-        cv2.line(img, (x5, y5), (x4, y4), color_blue, line_width)
-        cv2.line(img, (x4, y4), (x3, y3), color_blue, line_width)
-        break
 
+        color = color_blue
+        rect_color = color_green
+        if abs(y1 - y3) > 3:
+            color = color_red
+            rect_color = color_red
 
-    cv2.imshow('my webcam', img)
+        if abs(x5 - (x1 + x3) / 2) > 2.5:
+            color = color_red
+            rect_color = color_red
+
+        cv2.line(img, (x1, y1), (x2, y2), color, line_width)
+        cv2.line(img, (x2, y2), (x5, y5), color, line_width)
+        cv2.line(img, (x5, y5), (x4, y4), color, line_width)
+        cv2.line(img, (x4, y4), (x3, y3), color, line_width)
+        cv2.rectangle(img, (det.left(), det.top()), (det.right(), det.bottom()), rect_color, line_width)
+        #break
+
+    cv2.imshow('Face Detactor', img)
     if cv2.waitKey(1) == 27:
         break  # esc to quit
 cv2.destroyAllWindows()
